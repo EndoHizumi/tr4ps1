e22e4d8ea462ce9937e34f1da0a6d42964067ac2
Endo_Hizumi <endohizumi0114@gmail.com>
2020-07-28 01:59:59 +0900

    wip

--- functions/Write-Worktime.psm1
+++ functions/Write-Worktime.psm1
@@ -2,9 +2,9 @@ function Write-Worktime([String] $date, [String] $time, [String] $state) {
     [String] $data_directory = $env:attendance_directory
     [String] $file_path = (Join-Path $data_directory "worktime.csv")
 
-    if (-Not ( Test-Path $file_path)){
-        New-Item -ItemType "file" -Path $file_path
-        Write-Output "date,time,state" | Out-file $file_path -Encoding  UTF8 -Append
+    if (-Not ( Test-Path $data_directory)){
+        New-Item -ItemType "directory" -Path $data_directory
+        Write-Output "date,time,state" | Out-file $file_path -Encoding  UTF8
     }
     Write-Output "${date},${time},${state}" | Out-File $file_path -Encoding UTF8 -Append
 }
